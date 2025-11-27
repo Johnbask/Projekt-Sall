6ef225473ce56c31e9426fb0646c1b247fc2d442
jbski <johnbs1999@gmail.com>
2025-11-27 11:51:12 +0100

    Changes in Controller and Storage

--- SallWhiskySystem/src/storage/Storage.java
+++ SallWhiskySystem/src/storage/Storage.java
@@ -1,5 +1,6 @@
 package storage;
 
+import model.Fad;
 import model.Lager;
 
 import java.util.ArrayList;
@@ -8,6 +9,7 @@ import java.util.List;
 public class Storage {
 
     static List<Lager> lagere = new ArrayList<>();
+    static List<Fad> fade = new ArrayList<>();
 
     public static void addLager(Lager lager){
         lagere.add(lager);
@@ -18,5 +20,18 @@ public class Storage {
 
     public static List<Lager> getLagere() {
         return lagere;
+    }
+
+
+    public static List<Fad> getFade() {
+        return fade;
+    }
+
+    public static void addFade(Fad fad) {
+        fade.add(fad);
+    }
+
+    public static void removeFade(Fad fad) {
+        fade.remove(fad);
     }
 }
