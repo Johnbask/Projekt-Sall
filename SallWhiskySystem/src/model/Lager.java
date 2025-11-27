117623afac9f384cad49312f96a4e320b1ca1e0b
RuBangg <nielsruben91@gmail.com>
2025-11-27 12:29:43 +0100

    HyldeReolLager

--- SallWhiskySystem/src/model/Lager.java
+++ SallWhiskySystem/src/model/Lager.java
@@ -1,15 +1,37 @@
 package model;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Lager implements
         java.io.Serializable {
 
     private String adresse;
     private int LagerId;
     private  String navn;
+    private Map<Integer,Reol> reoler = new HashMap<>();
 
     public Lager(String adresse, int lagerId, String navn) {
         this.adresse = adresse;
         LagerId = lagerId;
         this.navn = navn;
     }
+
+    public void addReol(int x,Reol reol){
+        reoler.put(x,reol);
+    }
+
+    public Reol getReol(int x){
+        return reoler.get(x);
+    }
+
+    public void removeReol(int x){
+        reoler.remove(x);
+    }
+
+
+
+
+
 }
