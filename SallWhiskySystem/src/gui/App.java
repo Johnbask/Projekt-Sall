560c0665b44e7d497af0d9236b413e7143083277
Sander Frost <sanderfrost123@gmail.com>
2025-11-27 13:11:45 +0100

    workong on init storage

--- SallWhiskySystem/src/gui/App.java
+++ SallWhiskySystem/src/gui/App.java
@@ -17,13 +17,28 @@ public class App {
 
     public static void initStorage(){
 
-
+// opret lagere
         Lager SHlager = Controller.opretLager("Sønderhøj 30","Det første lager");
-        Controller.addReolerTilLager(SHlager,10);
-        Controller.addHylderTilReol(Controller.FindReolPåLager(SHlager),1);
-
-
         Lager SanderLager = Controller.opretLager("Serup Tinghøjvej 1", "Det bedste Lager");
+
+
+        // tilføj reoler og hylder til lagerne
+        Controller.addReolerTilLager(SHlager,10);
+        Controller.addReolerTilLager(SanderLager,4);
+        // FindReolPåLager tager et lager og en int, int svare til reolens plads i lagere. Den retunere den Reol
+        // addHylderTilReol tilføjere flere hylder til en reol
+        Controller.addHylderTilReol(Controller.FindReolPåLager(SHlager,1),5);
+        Controller.addHylderTilReol(Controller.FindReolPåLager(SHlager,2),4);
+        Controller.addHylderTilReol(Controller.FindReolPåLager(SHlager,3),3);
+        Controller.addHylderTilReol(Controller.FindReolPåLager(SHlager,4),7);
+
+
+        Controller.addHylderTilReol(Controller.FindReolPåLager(SanderLager,1),2);
+        Controller.addHylderTilReol(Controller.FindReolPåLager(SanderLager,2),2);
+        Controller.addHylderTilReol(Controller.FindReolPåLager(SanderLager,3),2);
+        Controller.addHylderTilReol(Controller.FindReolPåLager(SanderLager,4),2);
+
+
 
     }
 }
