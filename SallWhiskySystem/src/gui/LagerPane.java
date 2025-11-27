6fb46fc0c2e2dd13955059eeb5b522ce1b78a8fe
RuBangg <nielsruben91@gmail.com>
2025-11-27 13:03:53 +0100

    LagerPane

--- SallWhiskySystem/src/gui/LagerPane.java
+++ SallWhiskySystem/src/gui/LagerPane.java
@@ -24,15 +24,15 @@ public class LagerPane extends GridPane {
         this.add(lFade,0,0);
 
         this.add(tFlager,2,1);
-        Label lLager = new Label("Lager ");
+        Label lLager = new Label("Lager id");
         this.add(lLager,1,1);
 
         this.add(tFReol,2,2);
-        Label lReol = new Label("Reol ");
+        Label lReol = new Label("Reol id");
         this.add(lReol,1,2);
 
         this.add(tFHylde,2,3);
-        Label lHylde = new Label("Hylde ");
+        Label lHylde = new Label("Hylde id");
         this.add(lHylde,1,3);
 
         this.add(lWfade,0,1,1,6);
@@ -47,9 +47,7 @@ public class LagerPane extends GridPane {
         if(tFlager.getCharacters().isEmpty()){
             lWfade.getItems().setAll(Controller.getFade());
         } else if (!tFlager.getCharacters().isEmpty()&&!tFReol.getCharacters().isEmpty()&&!tFHylde.getCharacters().isEmpty()){
-
             lWfade.getItems().setAll(Controller.getLager(tFlager.getText()).getReol(Integer.parseInt(tFReol.getText())).getHylde(Integer.parseInt(tFHylde.getText())).getFade());
-
         } else if (tFHylde.getCharacters().isEmpty()) {
             lWfade.getItems().setAll(Controller.getLager(tFlager.getText()).getReol(Integer.parseInt(tFReol.getText())).getFade());
         } else if (!tFlager.getText().isEmpty()) {
